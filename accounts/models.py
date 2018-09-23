7feec4a312881e2fe7a56a41ebbd7760121fa444
Ashiri <ajaoiyanu@gmail.com>
2018-09-23 23:36:39 +0100

    [#4] create user accounts and authentication

---
+++ accounts/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.core.mail import send_mail
+from django.utils.translation import gettext_lazy as _
+
+from .managers import UserManager
+
+# Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(_('first_name'), max_length=200)
+    last_name = models.CharField(_('last_name'), max_length=200)
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        error_messages={
+            'unique': _("A user with that email already exists."),
+        },
+    )
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        abstract = True
+
+    def clean(self):
+        super().clean()
+
+    def get_first_name(self):
+        """
+        Return the first name
+        """
+        return self.first_name
+
+    def get_fullname(self):
+        """
+        Return the fullname
+        :return: fullname
+        """
+        fullname = '{} {}'.format(self.first_name, self.last_name)
+        return fullname
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Send an email to this user
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
